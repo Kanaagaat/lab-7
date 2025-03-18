3666cc6b8497e342ed7c54f3119c326427c70ae1
Kanagat <sauranbaykanagat@gmail.com>
2025-03-18 23:09:19 +0500

    change of hands

--- task1/2.py
+++ task1/2.py
@@ -2,90 +2,65 @@ import pygame
 import datetime
 import time
 
-
-def blitRotateCenter(surf, image, topleft, angle):
-
+def blitRotateCenter(surf, image, center, angle):
     rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
-
+    new_rect = rotated_image.get_rect(center=center)  
     surf.blit(rotated_image, new_rect)
 
 
-
-
-
-
-
-
 pygame.init()
-pygame.display.set_caption("Snake")
+pygame.display.set_caption("Mickey Clock")
 
 
-
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 font = pygame.font.Font("task1/font/Pixeltype.ttf", 100)
+
+
+mickey = pygame.image.load("task1/pics/main1.png").convert_alpha()
+mickey = pygame.transform.scale(mickey, (800, 400))
+
+right_hand = pygame.image.load("task1/pics/right-hand.png").convert_alpha()
+right_hand = pygame.transform.scale(right_hand, (200, 200))
+
+left_hand = pygame.image.load("task1/pics/left-hand.png").convert_alpha()
+left_hand = pygame.transform.scale(left_hand, (200, 200))
+
+
+clock_center = (400, 200)  
+
 done = False
 
-mickey = pygame.image.load("task1/pics/main1.png").convert()
-mickey_rect = mickey.get_rect(topleft = (0 , 0))
-
-right_hand = pygame.image.load("task1/pics/right-hand.png")
-right_rect = right_hand.get_rect(center = (400,400))
-
-left_hand = pygame.image.load("task1/pics/left-hand.png")
-left_rect = left_hand.get_rect(center = (400,400))
-
-
-
-
-
-DEFAULT_IMAGE_SIZE = (800, 400)
-mickey = pygame.transform.scale(mickey, DEFAULT_IMAGE_SIZE)
-right_hand = pygame.transform.scale(right_hand, (200,200))
-left_hand = pygame.transform.scale(left_hand, (200,200))
-
-
-
 while not done:
-    
-    time_of_clock = datetime.datetime.now().strftime(r"%M:%S")
-    tt = datetime.datetime.now()
-    min  = tt.minute
-    sec = tt.second
-    ang_min = -(6 * min + (sec / 10)) 
-    ang_sec = -(6 * sec) - 90
+   
+    now = datetime.datetime.now()
+    min = now.minute
+    sec = now.second
 
+   
+    ang_min = -(6 * min + sec / 10) 
+    ang_sec = -(6 * sec) 
 
+   
+    time_of_clock = now.strftime(r"%M:%S")
     text = font.render(time_of_clock, True, 'Green')
-    text_rect = text.get_rect(topleft = (100, 200)) 
+    text_rect = text.get_rect(topleft=(100, 200))  
+
   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-   
+    
+    screen.fill((255, 255, 255))
+    screen.blit(mickey, (0, 0))
 
-
-
-
-    print(F"{min} {sec}")
-
-
-    screen.blit(mickey, mickey_rect)
-    blitRotateCenter(screen, right_hand, (270,100), ang_min)
-    blitRotateCenter(screen, left_hand, (270,100), ang_sec)
-   
+    blitRotateCenter(screen, right_hand, clock_center, ang_min)  
+    blitRotateCenter(screen, left_hand, clock_center, ang_sec)  
 
     screen.blit(text, text_rect)
 
-
     pygame.display.flip()
-    pygame.display.update()
     clock.tick(60)
 
-
-
-
-
-
+pygame.quit()
